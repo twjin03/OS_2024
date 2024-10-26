863be9aa001fab1844530367e19f09ccd6203281
twjin03 <twjin03@g.skku.edu>
2024-10-27 03:06:02 +0900

    Update proc.h
    struct proc 수정

--- xv6-public/proc.h
+++ xv6-public/proc.h
@@ -51,10 +51,15 @@ struct proc {
   char name[16];               // Process name (debugging)
 
   int nice;  //(pa1)
-  
+
   int weight; //(pa2) 프로세스 가중치 
-  uint runtime; //(pa2) 총 런타임 
+
+  uint runtime_d_weight; //(pa2) ???
+  uint time_slice; //(pa2) ???
+
+  uint runtime; //(pa2) 총 런타임, 프로세스가 실제로 CPU를 사용한 시간
   uint vruntime; //(pa2) 가상 런타임
+  uint total_tick; //(pa2) 프로세스가 실행된 총 tick 수, 이 값은 프로세스의 실행 빈도와 관련 있음 
   
 
 };
